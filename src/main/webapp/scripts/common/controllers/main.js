1a2525f03f1e93c14cdc202cbfbbbbb89cf2ca85
Dewey Hong <doohee323@gmail.com>
2014-01-28 15:11:39 -0800

    remove useless files

--- src/main/webapp/scripts/common/controllers/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.controller('DefaultCtrl', function ($scope) {
-})
-.controller('GlobalCtrl', function ($scope, $location, $state) {
-  $scope.$state = $state;
-  $scope.$location = $location;
-});
-
